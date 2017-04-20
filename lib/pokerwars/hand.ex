0ed7ecc3656c241d2fe097539e427fd347d1f7d4
James Long <jamesharwoodlong@gmail.com>
2017-04-20 20:11:15 +0100

    Add evaluation function for all (size 5) hands

--- lib/pokerwars/hand.ex
+++ lib/pokerwars/hand.ex
@@ -2,8 +2,111 @@ defmodule Pokerwars.Hand do
   alias Pokerwars.Card
 
   def score(cards) do
-    evaluate(cards)
+    cards
+    |> Enum.sort()
+    |> evaluate()
   end
 
-  defp evaluate(_), do: :high_card
+  defp evaluate(hand) do
+    cond do
+      royal_flush?(hand) -> :royal_flush
+      straight_flush?(hand) -> :straight_flush
+      four_of_a_kind?(hand) -> :four_of_a_kind
+      full_house?(hand) -> :full_house
+      flush?(hand) -> :flush
+      straight?(hand) -> :straight
+      three_of_a_kind?(hand) -> :three_of_a_kind
+      two_pair?(hand) -> :two_pair
+      pair?(hand) -> :pair
+      true -> :high_card
+    end
+  end
+
+  defp royal_flush?(
+    [%Card{rank: 10, suit: suit},
+     %Card{rank: 11, suit: suit},
+     %Card{rank: 12, suit: suit},
+     %Card{rank: 13, suit: suit},
+     %Card{rank: 14, suit: suit}]
+  ), do: true
+  defp royal_flush?(_), do: false
+
+  defp straight_flush?(hand) do
+    straight?(hand) and flush?(hand)
+  end
+
+  defp four_of_a_kind?(hand) do
+    ranks = extract_ranks(hand)
+    case ranks do
+      [a, a, a, a, _b] -> true
+      [_a, b, b, b, b] -> true
+      _ -> false
+    end
+  end
+
+  defp full_house?(hand) do
+    ranks = extract_ranks(hand)
+    case ranks do
+      [a, a, b, b, b] -> true
+      [a, a, a, b, b] -> true
+      _ -> false
+    end
+  end
+
+  defp flush?(hand), do: matching_suit?(hand)
+
+  defp straight?(hand) do
+    hand
+    |> extract_ranks()
+    |> consecutive?()
+  end
+
+  defp three_of_a_kind?(hand) do
+    ranks = extract_ranks(hand)
+    case ranks do
+      [a, a, a, _b, _c] -> true
+      [_a, b, b, b, _c] -> true
+      [_a, _b, c, c, c] -> true
+      _ -> false
+    end
+  end
+
+  defp two_pair?(hand) do
+    ranks = extract_ranks(hand)
+    case ranks do
+      [a, a, b, b, _d] -> true
+      [a, a, _b, c, c] -> true
+      [_a, b, b, c, c] -> true
+      _ -> false
+    end
+  end
+
+  defp pair?(hand) do
+    ranks = extract_ranks(hand)
+    case ranks do
+      [a, a, _b, _c, _d] -> true
+      [_a, b, b, _c, _d] -> true
+      [_a, _b, c, c, _d] -> true
+      [_a, _b, _c, d, d] -> true
+      _ -> false
+    end
+  end
+
+  defp extract_ranks(hand) do
+    Enum.map(hand, &(&1.rank))
+  end
+
+  defp consecutive?([_a]), do: true
+  defp consecutive?([a | [b | t]]) do
+   a + 1 == b and consecutive?([b | t])
+  end
+
+  defp matching_suit?(
+    [%Card{rank: _, suit: suit},
+     %Card{rank: _, suit: suit},
+     %Card{rank: _, suit: suit},
+     %Card{rank: _, suit: suit},
+     %Card{rank: _, suit: suit}]
+  ), do: true
+  defp matching_suit?(_), do: false
 end
--- test/pokerwars/hand_test.exs
+++ test/pokerwars/hand_test.exs
@@ -40,6 +40,102 @@ defmodule Pokerwars.HandTest do
     assert Hand.score(cards) != :royal_flush
   end
 
+  test "evaluates straight flush 2-6 hearts" do
+    cards = [
+      %Card{rank: 2, suit: :hearts},
+      %Card{rank: 3, suit: :hearts},
+      %Card{rank: 4, suit: :hearts},
+      %Card{rank: 5, suit: :hearts},
+      %Card{rank: 6, suit: :hearts}
+    ]
+
+    assert Hand.score(cards) == :straight_flush
+  end
+
+  test "evaluates four of a kind of 4s" do
+    cards = [
+      %Card{rank: 2, suit: :hearts},
+      %Card{rank: 4, suit: :diamonds},
+      %Card{rank: 4, suit: :hearts},
+      %Card{rank: 4, suit: :clubs},
+      %Card{rank: 4, suit: :spades}
+    ]
+
+    assert Hand.score(cards) == :four_of_a_kind
+  end
+
+  test "evaluates full house 2s and 3s" do
+    cards = [
+      %Card{rank: 2, suit: :hearts},
+      %Card{rank: 2, suit: :diamonds},
+      %Card{rank: 3, suit: :diamonds},
+      %Card{rank: 3, suit: :clubs},
+      %Card{rank: 3, suit: :spades}
+    ]
+
+    assert Hand.score(cards) == :full_house
+  end
+
+  test "evaluates flush hearts" do
+    cards = [
+      %Card{rank: 2, suit: :hearts},
+      %Card{rank: 3, suit: :hearts},
+      %Card{rank: 4, suit: :hearts},
+      %Card{rank: 5, suit: :hearts},
+      %Card{rank: 7, suit: :hearts}
+    ]
+
+    assert Hand.score(cards) == :flush
+  end
+
+  test "evaluates straight 2-6" do
+    cards = [
+      %Card{rank: 2, suit: :hearts},
+      %Card{rank: 3, suit: :diamonds},
+      %Card{rank: 4, suit: :hearts},
+      %Card{rank: 5, suit: :spades},
+      %Card{rank: 6, suit: :clubs}
+    ]
+
+    assert Hand.score(cards) == :straight
+  end
+
+  test "evaluates 3 of a kind of 2s" do
+    cards = [
+      %Card{rank: 2, suit: :hearts},
+      %Card{rank: 2, suit: :diamonds},
+      %Card{rank: 2, suit: :hearts},
+      %Card{rank: 3, suit: :spades},
+      %Card{rank: 4, suit: :clubs}
+    ]
+
+    assert Hand.score(cards) == :three_of_a_kind
+  end
+
+  test "evaluates 2 pairs of 2s and 3s" do
+    cards = [
+      %Card{rank: 2, suit: :hearts},
+      %Card{rank: 3, suit: :diamonds},
+      %Card{rank: 12, suit: :hearts},
+      %Card{rank: 3, suit: :spades},
+      %Card{rank: 2, suit: :clubs}
+    ]
+
+    assert Hand.score(cards) == :two_pair
+  end
+
+  test "evaluates pair of 2s" do
+    cards = [
+      %Card{rank: 2, suit: :clubs},
+      %Card{rank: 11, suit: :diamonds},
+      %Card{rank: 12, suit: :hearts},
+      %Card{rank: 13, suit: :spades},
+      %Card{rank: 2, suit: :clubs}
+    ]
+
+    assert Hand.score(cards) == :pair
+  end
+
   test "evaluates high card" do
     cards = [
       %Card{rank: 2, suit: :spades},
